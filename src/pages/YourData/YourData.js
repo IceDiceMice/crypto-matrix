b9c417723f5e301d0efe1173d391dc45bf53d6e1
Dmytro Oliinyk <dmytro.oliinyk@gamelounge.com>
2022-11-24 19:04:06 +0200

    your-data page add

--- src/pages/YourData/YourData.js
+++ src/pages/YourData/YourData.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './YourData.scss'
+
 
  const YourData = () => {
   return (
-    <div>YourData</div>
+    <section className='your-data-body'>
+ <div className='your-data-header'>
+        <p className='your-data-title'>Ваші дані</p>
+        <p className='your-data-title'>Ваші дані</p>
+        <p className='your-data-title'>Ваші дані</p>
+        <div className='your-data-line'></div>
+  </div>
+    <div className='your-data-main'>
+      <p className='your-data-main-text'>Вашу заявку прийнято!<br/><br/>На ваш e-mail надіслано подальші інструкції!</p>
+      <p className='your-data-main-text'>Клікніть будь-де для повернення на головну</p>
+    </div>
+    </section>
   )
 }
 
